cedea2935182e6943bd15db3eebb989f3ece6de5
Shashank Gupta <shashankcm90@gmail.com>
2019-10-14 11:21:44 -0400

    Added Input and Hooks to handle textfield Change

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, ChangeEvent } from "react";
+import "./App.css";
 
-const App: React.FC = () => {
+interface IAppOwnProps {
+  username: string | undefined;
+  userType: "adim" | "moderator" | "user" | "guest";
+}
+
+const App: React.FC<IAppOwnProps> = ({ username, userType }): JSX.Element => {
+  const [time, setTime] = useState<Date>(() => new Date(Date.now()));
+  const [message, setMessage] = useState<string>("");
+
+  setInterval(() => {
+    setTime(new Date(Date.now()));
+  }, 1000);
+
+  const handleTextChange = (event: ChangeEvent<HTMLInputElement>): void => {
+    setMessage(event.target.value);
+  };
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>
+        Hi, {username ? username : "Mysterious Entity"}, your user type is{" "}
+        {username ? userType : "irrelevant because I do not know you"}.
+      </p>
+      <p>{time.toUTCString()}</p>
+      <input
+        type="text"
+        placeholder="Enter your message here"
+        value={message}
+        onChange={handleTextChange}
+      />
+      <p>Your message: {message || ""}</p>
     </div>
   );
-}
+};
 
 export default App;
